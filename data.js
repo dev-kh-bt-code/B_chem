802bf7cefda97e4a6e03843811cd0ead3c17e284
qb <quangbinh070408@gmail.com>
2025-06-24 22:52:04 +0700

    data+score

--- data.js
+++ data.js
@@ -1,18 +1,313 @@
 let amine =[
     {
-        img: "img/data/amine-thaythe-methanamine.svg",
+        img: "img/data/amine/01.N-methylbutan-1-amine.svg",
         q: "Tên thay thế của chất sau là gì",
-        a: "methanamine"
+        a: "n-methylbutan-1-amine"
     },
     {
-        img: "img/data/amine-thaythe-ethanamine.svg",
+        img: "img/data/amine/01.N-methylbutan-1-amine.svg",
+        q: "Tên gốc chức của chất sau là gì",
+        a: "butylmethylamine"
+    },
+    
+     {
+        img: "img/data/amine/02.N-ethyl-2,3-dimethylbutan-1-amine.svg",
+        q: "Tên thay thế của chất sau là gì",
+        a: "n-ethyl-2,3-dimethylbutan-1-amine"
+    },
+     {
+        img: "img/data/amine/03.N-ethyl-N-methyl-2,3-dimethylbutan-1-amine.svg",
+        q: "Tên thay thế của chất sau là gì",
+        a: "n-ethyl-n-methyl-2,3-dimethylbutan-1-amine"
+    },
+     {
+        img: "img/data/amine/04.N-ethyl-N-methylpropan-2-amine.svg",
+        q: "Tên thay thế của chất sau là gì",
+        a: "n-ethyl-n-methylpropan-2-amine"
+    },
+    {
+        img: "img/data/amine/04.N-ethyl-N-methylpropan-2-amine.svg",
+        q: "Tên gốc chức của chất sau là gì",
+        a: "ethylmethylisopropylamine"
+    },
+     {
+        img: "img/data/amine/05.N-ethyl-N-methylpropan-2-amine-vg.svg",
+        q: "Tên thay thế của chất sau là gì",
+        a: "n-ethyl-n-methylpropan-2-amine"
+    },
+    {
+        img: "img/data/amine/05.N-ethyl-N-methylpropan-2-amine-vg.svg",
+        q: "Tên gốc chức của chất sau là gì",
+        a: "ethylmethylisopropylamine"
+    },
+     {
+        img: "img/data/amine/06.N-ethyl-N-methyl-2-methylpropan-2-amine-vg.svg",
+        q: "Tên thay thế của chất sau là gì",
+        a: "n-ethyl-n-methyl-2-methylpropan-2-amine"
+    },
+    
+     {
+        img: "img/data/amine/07.N-ethyl-N-methyl-butan-2-amine.svg",
+        q: "Tên thay thế của chất sau là gì",
+        a: "n-ethyl-n-methyl-butan-2-amine"
+    },
+     {
+        img: "img/data/amine/08.N,N-diethyl-butan-2-amine.svg",
+        q: "Tên thay thế của chất sau là gì",
+        a: "n,n-diethyl-butan-2-amine"
+    },
+     {
+        img: "img/data/amine/09.N,N-diethyl-2,2-dimethylpropan-1-amine-vg.svg",
+        q: "Tên thay thế của chất sau là gì",
+        a: "n,n-diethyl-2,2-dimethylpropan-1-amine"
+    },
+     {
+        img: "img/data/amine/10.N,N-dimethylethenamine.svg",
+        q: "Tên thay thế của chất sau là gì",
+        a: "n,n-dimethylethenamine"
+    },
+     {
+        img: "img/data/amine/11.N-ethyl-propan-1-amine.svg",
+        q: "Tên thay thế của chất sau là gì",
+        a: "n-ethyl-propan-1-amine"
+    },
+    {
+        img: "img/data/amine/11.N-ethyl-propan-1-amine.svg",
+        q: "Tên gốc chức của chất sau là gì",
+        a: "ethylpropylamine"
+    },
+     {
+        img: "img/data/amine/12.N-ethylpropan-2-amine.svg",
+        q: "Tên thay thế của chất sau là gì",
+        a: "n-ethylpropan-2-amine"
+    },
+     {
+        img: "img/data/amine/13.N,N-dimethylpropan-1-amine.svg",
+        q: "Tên thay thế của chất sau là gì",
+        a: "n,n-dimethylpropan-1-amine"
+    },
+    {
+        img: "img/data/amine/13.N,N-dimethylpropan-1-amine.svg",
+        q: "Tên gốc chức của chất sau là gì",
+        a: "dimethylpropylamine"
+    },
+     {
+        img: "img/data/amine/14.N,N-dimethyl-propan-2-amine.svg",
+        q: "Tên thay thế của chất sau là gì",
+        a: "n,n-dimethyl-propan-2-amine"
+    },
+    
+     {
+        img: "img/data/amine/15.N-ethyl-N-methylethanamine.svg",
+        q: "Tên thay thế của chất sau là gì",
+        a: "n-ethyl-n-methylethanamine"
+    },
+    {
+        img: "img/data/amine/15.N-ethyl-N-methylethanamine.svg",
+        q: "Tên gốc chức của chất sau là gì",
+        a: "diethylmethylamine"
+    },
+     {
+        img: "img/data/amine/16.N-methylpropan-1-amine.svg",
+        q: "Tên thay thế của chất sau là gì",
+        a: "n-methylpropan-1-amine"
+    },
+    {
+        img: "img/data/amine/16.N-methylpropan-1-amine.svg",
+        q: "Tên gốc chức của chất sau là gì",
+        a: "methylpropylamine"
+    },
+     {
+        img: "img/data/amine/17.N-methylpropan-2-amine.svg",
+        q: "Tên thay thế của chất sau là gì",
+        a: "n-methylpropan-2-amine"
+    },
+     {
+        img: "img/data/amine/18.N,N-dimethylethanamine.svg",
+        q: "Tên thay thế của chất sau là gì",
+        a: "n,n-dimethylethanamine"
+    },
+    {
+        img: "img/data/amine/18.N,N-dimethylethanamine.svg",
+        q: "Tên gốc chức của chất sau là gì",
+        a: "ethyldimethylaimne"
+    },
+     {
+        img: "img/data/amine/19.N-ethylethanamine.svg",
+        q: "Tên thay thế của chất sau là gì",
+        a: "n-ethylethanamine"
+    },
+    {
+        img: "img/data/amine/19.N-ethylethanamine.svg",
+        q: "Tên gốc chức của chất sau là gì",
+        a: "diethylamine"
+    },
+     {
+        img: "img/data/amine/20.N,N-dimethylmethanamine.svg",
+        q: "Tên thay thế của chất sau là gì",
+        a: "n,n-dimethylmethanamine"
+    },
+    {
+        img: "img/data/amine/20.N,N-dimethylmethanamine.svg",
+        q: "Tên gốc chức của chất sau là gì",
+        a: "trimethylamine"
+    },
+     {
+        img: "img/data/amine/21.N,N-dimethylmethanamine-vg.svg",
+        q: "Tên thay thế của chất sau là gì",
+        a: "n,n-dimethylmethanamine"
+    },
+     {
+        img: "img/data/amine/22.N-methylethanamine.svg",
+        q: "Tên thay thế của chất sau là gì",
+        a: "n-methylethanamine"
+    },
+    {
+        img: "img/data/amine/22.N-methylethanamine.svg",
+        q: "Tên gốc chức của chất sau là gì",
+        a: "ethylmethylamine"
+    },
+     {
+        img: "img/data/amine/23.2-methylbutan-2-amine.svg",
+        q: "Tên thay thế của chất sau là gì",
+        a: "2-methylbutan-2-amine"
+    },
+     {
+        img: "img/data/amine/24.2,2-dimethylpropan-1-amine.svg",
+        q: "Tên thay thế của chất sau là gì",
+        a: "2,2-dimethylpropan-1-amine"
+    },
+     {
+        img: "img/data/amine/25.2-methylbutan-1-amine.svg",
+        q: "Tên thay thế của chất sau là gì",
+        a: "2-methylbutan-1-amine"
+    },
+     {
+        img: "img/data/amine/26.3-methylbutan-2-amine.svg",
+        q: "Tên thay thế của chất sau là gì",
+        a: "26.3-methylbutan-2-amine"
+    },
+     {
+        img: "img/data/amine/27.3-methylbutan-1-amine.svg",
+        q: "Tên thay thế của chất sau là gì",
+        a: "3-methylbutan-1-amine"
+    },
+     {
+        img: "img/data/amine/28.2-methylbutan-1-amine.svg",
+        q: "Tên thay thế của chất sau là gì",
+        a: "2-methylbutan-1-amine"
+    },
+     
+     {
+        img: "img/data/amine/29.pentan-2-amine.svg",
+        q: "Tên thay thế của chất sau là gì",
+        a: "pentan-2-amine"
+    },
+     {
+        img: "img/data/amine/30.pentan-1-amine.svg",
+        q: "Tên thay thế của chất sau là gì",
+        a: "pentan-1-amine"
+    },
+     {
+        img: "img/data/amine/31.2-methylpropan-2-amine.svg",
+        q: "Tên thay thế của chất sau là gì",
+        a: "2-methylpropan-2-amine"
+    },
+    {
+        img: "img/data/amine/31.2-methylpropan-2-amine.svg",
+        q: "Tên gốc chức của chất sau là gì",
+        a: "tert-butylamine"
+    },
+     {
+        img: "img/data/amine/32.2-methylpropan-1-amine.svg",
+        q: "Tên thay thế của chất sau là gì",
+        a: "2-methylpropan-1-amine"
+    },
+    {
+        img: "img/data/amine/32.2-methylpropan-1-amine.svg",
+        q: "Tên gốc chức của chất sau là gì",
+        a: "isobutylamine"
+    },
+     {
+        img: "img/data/amine/33.butan-2-amine.svg",
+        q: "Tên thay thế của chất sau là gì",
+        a: "butan-2-amine"
+    },
+    {
+        img: "img/data/amine/33.butan-2-amine.svg",
+        q: "Tên gốc chức của chất sau là gì",
+        a: "sec-butylamine"
+    },
+     {
+        img: "img/data/amine/34.butan-1-amine.svg",
+        q: "Tên thay thế của chất sau là gì",
+        a: "butan-1-amine"
+    },
+    {
+        img: "img/data/amine/34.butan-1-amine.svg",
+        q: "Tên gốc chức của chất sau là gì",
+        a: "butylamine"
+    },
+     {
+        img: "img/data/amine/35.propan-2-amine.svg",
+        q: "Tên thay thế của chất sau là gì",
+        a: "propan-2-amine"
+    },
+     {
+        img: "img/data/amine/35.propan-2-amine.svg",
+        q: "Tên gốc chức của chất sau là gì",
+        a: "isopropylamine"
+    },
+    
+     {
+        img: "img/data/amine/36.propan-1-amine.svg",
+        q: "Tên thay thế của chất sau là gì",
+        a: "propan-1-amine"
+    },
+     {
+        img: "img/data/amine/36.propan-1-amine.svg",
+        q: "Tên gốc chức của chất sau là gì",
+        a: "propylamine"
+    },
+     {
+        img: "img/data/amine/37.ethanamine.svg",
         q: "Tên thay thế của chất sau là gì",
         a: "ethanamine"
     },
-    {
-        img: "img/data/amine-thaythe-propan-1-amine.svg",
+     {
+        img: "img/data/amine/37.ethanamine.svg",
+        q: "Tên gốc chức của chất sau là gì",
+        a: "ethylamine"
+    },
+     {
+        img: "img/data/amine/38.methanamine.svg",
         q: "Tên thay thế của chất sau là gì",
-        a: "propan-1-amine"
+        a: "methanamine"
+    },
+     {
+        img: "img/data/amine/38.methanamine.svg",
+        q: "Tên gốc chức của chất sau là gì",
+        a: "methylamine"
+    },
+    {
+        img: "img/data/amine/39.benzenamine-vg.svg",
+        q: "Tên thay thế của chất sau là gì",
+        a: "benzeneamine"
+    },
+    {
+        img: "img/data/amine/40.benzenamine.svg",
+        q: "Tên thay thế của chất sau là gì",
+        a: "benzeneamine"
+    },
+    {
+        img: "img/data/amine/40.benzenamine.svg",
+        q: "Tên gốc chức của chất sau là gì",
+        a: "phenylamine"
+    },
+    {
+        img: "img/data/amine/40.benzenamine.svg",
+        q: "Tên thường của chất sau là gì",
+        a: "aniline"
     },
 ];
 localStorage.removeItem("Data");
--- test.js
+++ test.js
@@ -1,15 +1,24 @@
 let arr=JSON.parse(localStorage.getItem("Data")) || [];
 let messages_true = [
     "Đúng roiii",
-    "Quá đúng luôn"
+    "Quá đúng luôn",
+    "Giỏi dữ tr",
+    "good job",
+    "Tiếp luôn bn ei"
 ];
 let messages_false=[
     "Sai roi bn ei",
-    "Sai nữa roi"
+    "Sai nữa roi",
+    "Làm lại đi sai roi",
+    "Tí nữa là đúng roi"
 ];
 
 
-let i = 0;
+
+let max_ques = 10;
+let ques = 0;
+let score = 0;
+    let i = 0;
 
 let btnnext =document.getElementById("btn-next");
 let random;
@@ -18,20 +27,27 @@ btnnext.addEventListener("click", ()=> {
     document.getElementById("test-question").innerText= arr[random].q;
     document.getElementById("test-img").src= arr[random].img;
     i = i+1;
-    document.getElementById("test-h1").innerText ="Câu " + i;
-    document.getElementById("a-input").value=""
-    document.getElementById("a-output").innerText = ""
+    ques = ques + 1;
+    document.getElementById("test-h1").innerText ="Câu " + ques;
+    document.getElementById("a-input").value="";
+    document.getElementById("a-output").innerText = "";
     
-});    
+    if (ques == max_ques) {
+    localStorage.setItem("score", score);
+    window.location.href = "score.html";
 
-let btncheck = document.getElementById("btn-check");
+};
+});    
+    let btncheck = document.getElementById("btn-check");
 btncheck.addEventListener("click",()=>{
-    let ans = document.getElementById("a-input").value.toLowerCase().replace(/\s/g, "");
-    if (ans == arr[random].a) {
+    
+    let ans = document.getElementById("a-input").value.toLowerCase().replace(/[.,\- \s]/g, "");
+    let ans_check= arr[random].a.toLowerCase().replace(/[.,\- \s]/g, "");
+    if (ans == ans_check) {
         let random_messages_true= Math.floor(Math.random()*messages_true.length);
 
         document.getElementById("a-output").innerText = messages_true[random_messages_true];
-        arr.splice(random,1);
+        score= score+1;
 
     }else{
         let random_messages_false= Math.floor(Math.random()*messages_false.length);
@@ -43,6 +59,7 @@ let btnkey = document.getElementById("btn-key");
 btnkey.addEventListener("click", ()=>{
     document.getElementById("a-output").innerText = "Đáp án là " + arr[random].a;
 });
+
 
     
 
